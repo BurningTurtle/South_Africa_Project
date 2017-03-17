01190ca8885c694904e4e99452db02e8f295d496
Moritz <moritz.unseld@googlemail.com>
2017-03-17 18:42:01 +0100

    Added grass and fence

---
+++ Assets/Resources/Scripts/MainSceneController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainSceneController : MonoBehaviour {
+
+	public int gridRows = 10;
+	public int gridCols = 10;
+	public int offset = 1;
+	public Grass grass0, grass1, grass2, grass3;
+	public Grass grassBlocked0, grassBlocked1, grassBlocked2, grassBlocked3;
+
+	// Use this for initialization
+	void Start () {
+		Vector2 startPos = new Vector2(0,0);
+
+		for (int i = 0; i < gridCols; i++) {
+			for (int j = 0; j < gridRows; j++) {
+				Grass grass = Instantiate (grass0);
+				int id = Random.Range (0, 3);
+				switch (id) {
+				case 0:
+					if (i == 0 || j == 0 || i == gridCols-1 || j == gridRows-1) {
+						grass = Instantiate (grassBlocked0) as Grass;
+					} else {
+						grass = Instantiate (grass0) as Grass;
+					}
+					break;
+				case 1:
+					if (i == 0 || j == 0 || i == gridCols-1 || j == gridRows-1) {
+						grass = Instantiate (grassBlocked1) as Grass;
+					} else {
+						grass = Instantiate (grass1) as Grass;
+					}
+					break;
+				case 2:
+					if (i == 0 || j == 0 || i == gridCols-1 || j == gridRows-1) {
+						grass = Instantiate (grassBlocked2) as Grass;
+					} else {
+						grass = Instantiate (grass2) as Grass;
+					}
+					break;
+				case 3:
+					if (i == 0 || j == 0 || i == gridCols-1 || j == gridRows-1) {
+						grass = Instantiate (grassBlocked3) as Grass;
+					} else {
+						grass = Instantiate (grass3) as Grass;
+					}
+					break;
+				}
+				
+				float posX = (offset * i) + startPos.x;
+				float posY = (offset * j) + startPos.y;
+				grass.transform.position = new Vector3 (posX, posY, 100);
+			}
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Resources/Scripts/PlayerScript.cs
+++ Assets/Resources/Scripts/PlayerScript.cs
@@ -8,14 +8,17 @@ public class PlayerScript : MonoBehaviour {
 
 	public float playerSpeed = 4f;
 	private Animator animator;
+	public MainSceneController msc;
 
 	// Use this for initialization
 	void Start () {
 		animator = this.GetComponent<Animator> ();
+		transform.position = new Vector2 (msc.gridCols / 2 - 0.5f, msc.gridRows / 2);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		transform.rotation = Quaternion.Euler (0, 0, 0); 
 		if (Input.GetAxisRaw ("Horizontal") != 0 || Input.GetAxisRaw ("Vertical") != 0) {
 			animator.SetBool ("Walking", true);
 		} else {
