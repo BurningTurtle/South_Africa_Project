3829e29a71a51d48ae0b25d370e321cc0566036b
Moritz <moritz.unseld@googlemail.com>
2017-03-21 18:08:43 +0100

    Added animated fountain

--- Assets/Resources/Scripts/PlayerScript.cs
+++ Assets/Resources/Scripts/PlayerScript.cs
@@ -18,7 +18,7 @@ public class PlayerScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.rotation = Quaternion.Euler (0, 0, 0); 
+		//transform.rotation = Quaternion.Euler (0, 0, 0); 
 		if (Input.GetAxisRaw ("Horizontal") != 0 || Input.GetAxisRaw ("Vertical") != 0) {
 			animator.SetBool ("Walking", true);
 		} else {
