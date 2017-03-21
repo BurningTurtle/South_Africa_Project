45b5366053d9552b2ad4664b44b6edb36c5337bf
Moritz <moritz.unseld@googlemail.com>
2017-03-21 22:24:39 +0100

    Added karma UI and polished fountain system

--- Assets/Resources/Scripts/Fountain.cs
+++ Assets/Resources/Scripts/Fountain.cs
@@ -5,18 +5,37 @@ using UnityEngine;
 public class Fountain : MonoBehaviour {
 
 	private Animator animator;
+	public int karma = 0;
+	[Header("Time in s")]
+	[SerializeField] private float period;
+	[SerializeField] private UIController uic;
+	private int elapsedSinceAction;
 
 	// Use this for initialization
 	void Start () {
 		animator = this.GetComponent<Animator> ();
+		InvokeRepeating ("KarmaAction", 0, period);
+		InvokeRepeating ("timeUntilAction", 0, 1);
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+	void timeUntilAction() {
+		if (elapsedSinceAction > period)
+			elapsedSinceAction = 1;
+		elapsedSinceAction += 1;
+		//uic.actionLabel.text = (period - elapsedSinceAction + 1).ToString ();
+	}
+		
+	void KarmaAction() {
+		karma += 1;
+		setState (karma);
 	}
 
 	public void setState(int state){
 		animator.SetInteger ("state", state);
+	}
+
+	// Update is called once per frame
+	void Update () {
 	}
 			
 		
--- Assets/Resources/Scripts/UIController.cs
+++ Assets/Resources/Scripts/UIController.cs
@@ -6,22 +6,18 @@ using UnityEngine;
 public class UIController : MonoBehaviour {
 
 	[SerializeField] private Text karmaLabel;
-	[SerializeField] private Text actionLabel;
 	[SerializeField] private Fountain fountain;
-	public int karma = 0;
+	public Text actionLabel;
+
 
 	// Use this for initialization
 	void Start () {
-		InvokeRepeating ("KarmaAction", 0, 1.0f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		karmaLabel.text = karma.ToString ();
+		karmaLabel.text = fountain.karma.ToString ();
 	}
 
-	void KarmaAction() {
-		karma += 1;
-		fountain.setState (karma);
-	}
+
 }
