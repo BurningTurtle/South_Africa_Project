c02f2f27661edc6f63b2837af6a7bbee827b6153
Moritz <moritz.unseld@googlemail.com>
2017-03-21 18:24:47 +0100

    Added karma text and cleaned scene view

---
+++ Assets/Resources/Scripts/UIController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour {
+
+	[SerializeField] private Text scoreLabel;
+
+	// Use this for initialization
+	void Start () {
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		scoreLabel.text = Time.realtimeSinceStartup.ToString ();
+	}
+}
