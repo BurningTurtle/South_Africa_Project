28a3679ac008e7b1c2867964f7b9c084f0b63c1f
Moritz <moritz.unseld@googlemail.com>
2017-03-23 20:31:54 +0100

    Added water variable, correlated karma to it and implemented fading animations when the amount of water is changing

--- Assets/Resources/Scripts/Fountain.cs
+++ Assets/Resources/Scripts/Fountain.cs
@@ -2,41 +2,185 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Fountain : MonoBehaviour {
+public class Fountain : MonoBehaviour
+{
 
-	private Animator animator;
-	public int karma = 0;
-	[Header("Time in s")]
-	[SerializeField] private float period;
-	[SerializeField] private UIController uic;
-	private int elapsedSinceAction;
+    private Animator animator;
+    public int karma;
+    [HideInInspector]
+    public int water = 0;
+    public UIController uic;
+    [Header("Time in s")]
+    [SerializeField]
+    private float period;
+    private int elapsedSinceAction;
 
-	// Use this for initialization
-	void Start () {
-		animator = this.GetComponent<Animator> ();
-		InvokeRepeating ("KarmaAction", 0, period);
-		InvokeRepeating ("timeUntilAction", 0, 1);
-	}
+    // Use this for initialization
+    void Start()
+    {
+        animator = this.GetComponent<Animator>();
+        InvokeRepeating("KarmaAction", 0, period);
+        InvokeRepeating("timeUntilAction", 0, 1);
+    }
 
-	void timeUntilAction() {
-		if (elapsedSinceAction > period)
-			elapsedSinceAction = 1;
-		elapsedSinceAction += 1;
-		//uic.actionLabel.text = (period - elapsedSinceAction + 1).ToString ();
-	}
-		
-	void KarmaAction() {
-		karma += 1;
-		setState (karma);
-	}
+    void timeUntilAction()
+    {
+        if (elapsedSinceAction > period)
+            elapsedSinceAction = 1;
+        elapsedSinceAction += 1;
+    }
 
-	public void setState(int state){
-		animator.SetInteger ("state", state);
-	}
+    void KarmaAction()
+    {
+        int random = Random.Range(1, 10);
+        switch (karma)
+        {
+            case -5:
+                if (random < 2)
+                {
+                    water += 1;
+                    uic.fadePositive();
+                }
+                else
+                {
+                    water -= 1;
+                    uic.fadeNegative();
+                }
+                break;
 
-	// Update is called once per frame
-	void Update () {
-	}
-			
-		
+            case -4:
+                if (random < 3)
+                {
+                    water += 1;
+                    uic.fadePositive();
+                }
+                else
+                {
+                    water -= 1;
+                    uic.fadeNegative();
+                }
+                break;
+
+            case -3:
+                if (random < 4)
+                {
+                    water += 1;
+                    uic.fadePositive();
+                }
+                else
+                {
+                    water -= 1;
+                    uic.fadeNegative();
+                }
+                break;
+
+            case -2:
+                if (random < 5)
+                {
+                    water += 1;
+                    uic.fadePositive();
+                }
+                else
+                {
+                    water -= 1;
+                    uic.fadeNegative();
+                }
+                break;
+
+            case -1:
+                if (random < 6)
+                {
+                    water += 1;
+                    uic.fadePositive();
+                }
+                else
+                {
+                    water -= 1;
+                    uic.fadeNegative();
+                }
+                break;
+
+            case 1:
+                if (random < 7)
+                {
+                    water += 1;
+                    uic.fadePositive();
+                }
+                else
+                {
+                    water -= 1;
+                    uic.fadeNegative();
+                }
+                break;
+
+            case 2:
+                if (random < 8)
+                {
+                    water += 2;
+                    uic.fadePositive();
+                }
+                else
+                {
+                    water -= 1;
+                    uic.fadeNegative();
+                }
+                break;
+
+            case 3:
+                if (random < 9)
+                {
+                    water += 3;
+                    uic.fadePositive();
+                }
+                else
+                {
+                    water -= 1;
+                    uic.fadeNegative();
+                }
+                break;
+
+            case 4:
+                if (random < 10)
+                {
+                    water += 4;
+                    uic.fadePositive();
+                }
+                else
+                {
+                    water -= 1;
+                    uic.fadeNegative();
+                }
+                break;
+
+            case 5:
+                if (random < 11)
+                {
+                    water += 5;
+                    uic.fadePositive();
+                }
+                else
+                {
+                    water -= 1;
+                    uic.fadeNegative();
+                }
+                break;
+        }
+
+    }
+
+    public void setState(int state)
+    {
+        animator.SetInteger("state", state);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        setState((water - (water % 10)) / 10);
+        Debug.Log("Water: " + water);
+        Debug.Log("Karma: " + karma);
+    }
+
+
+
 }
--- Assets/Resources/Scripts/UIController.cs
+++ Assets/Resources/Scripts/UIController.cs
@@ -7,17 +7,43 @@ public class UIController : MonoBehaviour {
 
 	[SerializeField] private Text karmaLabel;
 	[SerializeField] private Fountain fountain;
-	public Text actionLabel;
+    [SerializeField] private Text positive, negative;
 
 
 	// Use this for initialization
 	void Start () {
-	}
+        positive.CrossFadeAlpha(0f, 0f, false);
+        negative.CrossFadeAlpha(0f, 0f, false);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		karmaLabel.text = fountain.karma.ToString ();
+	    karmaLabel.text = fountain.karma.ToString ();
 	}
+
+    public void fadePositive()
+    {
+        StartCoroutine(fadePositiveE());
+    }
+
+    IEnumerator fadePositiveE()
+    {
+        positive.CrossFadeAlpha(1f, 2f, false);
+        yield return new WaitForSeconds(2f);
+        positive.CrossFadeAlpha(0f, 2f, false);
+    }
+
+    public void fadeNegative()
+    {
+        StartCoroutine(fadeNegativeE());
+    }
+
+    IEnumerator fadeNegativeE()
+    {
+        negative.CrossFadeAlpha(1f, 2f, false);
+        yield return new WaitForSeconds(2f);
+        negative.CrossFadeAlpha(0f, 2f, false);
+    }
 
 
 }
