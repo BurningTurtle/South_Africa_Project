60d1d8b2325164e4a72d60f8d45691fd98d8c18c
Moritz <moritz.unseld@googlemail.com>
2017-04-11 15:02:19 +0200

    Added 7 questions (geo and ff now complete) and made the exits lovelier

--- Assets/Resources/Scripts/QuestionController.cs
+++ Assets/Resources/Scripts/QuestionController.cs
@@ -8,6 +8,9 @@ public class QuestionController : MonoBehaviour {
     [SerializeField] private Text question, answers, right, wrong;
     private bool answered = false;
     private int answer;
+    bool geo1ans, geo2ans, geo3ans, geo4ans;
+    bool ff1ans, ff2ans, ff3ans, ff4ans;
+    string currentQuestion;
 
     // Use this for initialization
     void Start () {
@@ -24,9 +27,77 @@ public class QuestionController : MonoBehaviour {
             switch(this.transform.name)
             {
                 case "geography1":
-                    question.text = "Which South African neighbor country is bordered by the river Orange";
-                    answers.text = "1 Namibia   2 Botswana   3 Mosambik   4 Simbabwe";
-                    StartCoroutine(getAnswer(1));
+                    if (!geo1ans)
+                    {
+                        currentQuestion = "geography1";
+                        question.text = "Which South African neighbor country is bordered by the river Orange";
+                        answers.text = "1 Namibia   2 Botswana   3 Mosambik   4 Simbabwe";
+                        StartCoroutine(getAnswer(1));
+                    }
+                    break;
+                case "geography2":
+                    if (!geo2ans)
+                    {
+                        currentQuestion = "geography2";
+                        question.text = "Which ocean borders South Africa";
+                        answers.text = "1 Pacific   2 Barents Sea   3 Atlantic   4 Artic";
+                        StartCoroutine(getAnswer(3));
+                    }
+                    break;
+                case "geography3":
+                    if (!geo3ans)
+                    {
+                        currentQuestion = "geography3";
+                        question.text = "Which of the following is the longest river in South Africa";
+                        answers.text = "1 Orange River   2 Red River   3 Blue River   4 None of them";
+                        StartCoroutine(getAnswer(1));
+                    }
+                    break;
+                case "geography4":
+                    if (!geo4ans)
+                    {
+                        currentQuestion = "geography4";
+                        question.text = "What is the southernmost point of the African continent";
+                        answers.text = "1 Cape Branco   2 Cape of Good Hope   3 Cape Corrientes   4 Cape Agulhas";
+                        StartCoroutine(getAnswer(4));
+                    }
+                    break;
+
+                case "ff1":
+                    if (!ff1ans)
+                    {
+                        currentQuestion = "ff1";
+                        question.text = "What can you admire in the Auckland Nature Reserve at Hogsback";
+                        answers.text = "1 Many many different plants   2 The world's largest bloom   3 Penguins   4 Original South African forest";
+                        StartCoroutine(getAnswer(4));
+                    }
+                    break;
+                case "ff2":
+                    if (!ff2ans)
+                    {
+                        currentQuestion = "ff2";
+                        question.text = "How is the vegetation in the north-west of South Africa";
+                        answers.text = "1 Many lions   2 Sparse   3 Species-rich   4 Only baobabs";
+                        StartCoroutine(getAnswer(3));
+                    }
+                    break;
+                case "ff3":
+                    if (!ff3ans)
+                    {
+                        currentQuestion = "ff3";
+                        question.text = "What type of rhinoceros are there in South Africa";
+                        answers.text = "1 White rhino   2 One-horned rhino   3 Java rhino   4 Great Matiko rhino";
+                        StartCoroutine(getAnswer(1));
+                    }
+                    break;
+                case "ff4":
+                    if (!ff4ans)
+                    {
+                        currentQuestion = "ff4";
+                        question.text = "Why are many species extinct in South Africa";
+                        answers.text = "1 Dryness   2 Too much rainfall   3 Insertion of European species   4 Buffaloes eat everything";
+                        StartCoroutine(getAnswer(3));
+                    }
                     break;
             }   
         }
@@ -65,6 +136,34 @@ public class QuestionController : MonoBehaviour {
         if (answer == rightAnswerInt)
         {
             StartCoroutine(rightAnswer());
+            switch(currentQuestion)
+            {
+                case "geography1":
+                    geo1ans = true;
+                    break;
+                case "geography2":
+                    geo2ans = true;
+                    break;
+                case "geography3":
+                    geo3ans = true;
+                    break;
+                case "geography4":
+                    geo4ans = true;
+                    break;
+
+                case "ff1":
+                    ff1ans = true;
+                    break;
+                case "ff2":
+                    ff2ans = true;
+                    break;
+                case "ff3":
+                    ff3ans = true;
+                    break;
+                case "ff4":
+                    ff4ans = true;
+                    break;
+            }
         }
         else
         {
@@ -76,7 +175,7 @@ public class QuestionController : MonoBehaviour {
     IEnumerator rightAnswer()
     {
         right.CrossFadeAlpha(1f, 2f, false);
-        GameObject.Find("Fountain").GetComponent<Fountain>().karma += 5; //CHANGE!
+        GameObject.Find("Fountain").GetComponent<Fountain>().karma += 1;
         yield return new WaitForSeconds(2f);
         right.CrossFadeAlpha(0f, 2f, false);
     }
--- Assets/Resources/Scripts/Teleport.cs
+++ Assets/Resources/Scripts/Teleport.cs
@@ -21,13 +21,13 @@ public class Teleport : MonoBehaviour {
             switch(this.transform.name)
             {
                 case "beehive0":
-                    other.transform.position = new Vector2(10, 38.5f);
+                    other.transform.position = new Vector2(10, 36.5f);
                     break;
                 case "exit1":
                     other.transform.position = new Vector2(4, 8.5f);
                     break;
                 case "beehive1":
-                    other.transform.position = new Vector2(60, 38.5f);
+                    other.transform.position = new Vector2(60, 36.5f);
                     break;
                 case "exit2":
                     other.transform.position = new Vector2(16, 8.5f);
